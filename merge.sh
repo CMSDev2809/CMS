ce290d431491df08e0827dd2c383c943550e8123
broberts2 <broc.roberts@outlook.com>
2019-12-23 10:48:00 -0700

    changes

--- merge.sh
+++ merge.sh
@@ -1,1 +1,9 @@
-git pull origin master; cd /CMS/compliancemonitoringsystems.com; npm run build; rm -rf /var/www/compliancemonitoringsystems.com/build; mv build /var/www/compliancemonitoringsystems.com; cd /CMS/communitysupervision.org; npm run build; rm -rf /var/www/communitysupervision.org/build; mv build /var/www/communitysupervision.org
+git pull origin master
+cd /CMS/compliancemonitoringsystems.com
+npm run build
+rm -rf /var/www/compliancemonitoringsystems.com/build
+mv build /var/www/compliancemonitoringsystems.com
+cd /CMS/communitysupervision.org
+npm run build
+rm -rf /var/www/communitysupervision.org/build
+mv build /var/www/communitysupervision.org
