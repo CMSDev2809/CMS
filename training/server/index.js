9c0e1e2f461d4670beef583d1e2feb8110fc61bc
broberts2 <broc.roberts@outlook.com>
2020-03-20 14:45:29 -0600

    changes

--- training/server/index.js
+++ training/server/index.js
@@ -11,8 +11,18 @@ const path = require("path");
 
 app.use(bodyParser.json());
 app.use("/Forms", express.static(config.base));
-app.use(cors({ origin: true }));
-secure.use(cors({ origin: true, credentials: true }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin, Authorization, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  next();
+});
+
 secure.use(security);
 
 app.use("/secure", secure);
