72a86e164580e5c374424a9f4ef0ff2fa8a1f55f
broberts2 <broc.roberts@outlook.com>
2020-03-24 15:30:53 -0600

    changes

--- training/src/components/selectors/selectors.js
+++ training/src/components/selectors/selectors.js
@@ -59,7 +59,7 @@ export default function Selectors(props) {
       <Card
         onClick={() =>
           el.url
-            ? el.toLowerCase() === "link"
+            ? el.url.toLowerCase().includes(".link")
               ? window.open(el.url, "_blank")
               : openContent(el)
             : props.setNode(el.children, el.title)
