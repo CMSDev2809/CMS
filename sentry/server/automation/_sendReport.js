c5eb4101c584c3a6702f839d7a3865e1f003d8ec
broberts2 <broc.roberts@outlook.com>
2020-06-04 10:53:31 -0600

    changes

--- sentry/server/automation/_sendReport.js
+++ sentry/server/automation/_sendReport.js
@@ -3,40 +3,44 @@ const Handler = require("../controllers/handler");
 const _Util = require("../controllers/_util");
 
 module.exports = async (accessionId) => {
-  const metaData = await Handler.Api.getResults({
-    query: { accessionId },
-  })
-    .then(
-      (res) =>
-        res.getResultsResponse.ResultRecords.AccessionRecords.AccessionRecord
-    )
-    .then((res) => ({
-      memo: res.EnrolleeRecord.Memo._text,
-      nameFirst: res.EnrolleeRecord.NameFirst._text,
-      nameLast: res.EnrolleeRecord.NameLast._text,
-      date: res.ResultDateTime._text,
-      abnormal: res.Abnormal._text,
-    }));
-  const content = await Handler.Api.getAccessionPDF({
-    query: { accessionId },
-  });
-  _Util.parseRecipients(metaData.memo).map((to) =>
-    _sendMail({
-      to,
-      abnormal: metaData.abnormal,
-      date: metaData.date,
-      nameFirst: metaData.nameFirst,
-      nameLast: metaData.nameLast,
-      subject: `${metaData.nameLast}, ${metaData.nameFirst} - Test Results`,
-      metaData,
-      attachments: [
-        {
-          encoding: "base64",
-          filename: `${metaData.nameLast}, ${metaData.nameFirst}.pdf`,
-          content,
-          contentType: "application/pdf",
-        },
-      ],
+  try {
+    const metaData = await Handler.Api.getResults({
+      query: { accessionId },
     })
-  );
+      .then(
+        (res) =>
+          res.getResultsResponse.ResultRecords.AccessionRecords.AccessionRecord
+      )
+      .then((res) => ({
+        memo: res.EnrolleeRecord.Memo._text,
+        nameFirst: res.EnrolleeRecord.NameFirst._text,
+        nameLast: res.EnrolleeRecord.NameLast._text,
+        date: res.ResultDateTime._text,
+        abnormal: res.Abnormal._text,
+      }));
+    const content = await Handler.Api.getAccessionPDF({
+      query: { accessionId },
+    });
+    _Util.parseRecipients(metaData.memo).map((to) =>
+      _sendMail({
+        to,
+        abnormal: metaData.abnormal,
+        date: metaData.date,
+        nameFirst: metaData.nameFirst,
+        nameLast: metaData.nameLast,
+        subject: `${metaData.nameLast}, ${metaData.nameFirst} - Test Results`,
+        metaData,
+        attachments: [
+          {
+            encoding: "base64",
+            filename: `${metaData.nameLast}, ${metaData.nameFirst}.pdf`,
+            content,
+            contentType: "application/pdf",
+          },
+        ],
+      })
+    );
+  } catch (e) {
+    console.log(e);
+  }
 };
--- sentry/server/automation/_sendViolation.js
+++ sentry/server/automation/_sendViolation.js
@@ -5,38 +5,42 @@ const pdf = require("html-pdf");
 const _violationPDF = require("./_violationPDF");
 
 module.exports = async (enrollee, violation) => {
-  enrollee = {
-    violation,
-    date: enrollee.TestDate._text,
-    nameFirst: enrollee.EnrolleeRecord.NameFirst._text,
-    nameLast: enrollee.EnrolleeRecord.NameLast._text,
-    memo: enrollee.EnrolleeRecord.Memo
-      ? enrollee.EnrolleeRecord.Memo._text
-      : "",
-    enrolleeCaseId: enrollee.EnrolleeRecord.EnrolleeCaseId
-      ? enrollee.EnrolleeRecord.EnrolleeCaseId._text
-      : "",
-  };
-  _Util.parseRecipients(enrollee.memo).map(async (to) => {
-    const content = await new Promise((resolve, reject) => {
-      pdf.create(_violationPDF(enrollee)).toBuffer((err, buffer) => {
-        resolve(buffer);
+  try {
+    enrollee = {
+      violation,
+      date: enrollee.TestDate._text,
+      nameFirst: enrollee.EnrolleeRecord.NameFirst._text,
+      nameLast: enrollee.EnrolleeRecord.NameLast._text,
+      memo: enrollee.EnrolleeRecord.Memo
+        ? enrollee.EnrolleeRecord.Memo._text
+        : "",
+      enrolleeCaseId: enrollee.EnrolleeRecord.EnrolleeCaseId
+        ? enrollee.EnrolleeRecord.EnrolleeCaseId._text
+        : "",
+    };
+    _Util.parseRecipients(enrollee.memo).map(async (to) => {
+      const content = await new Promise((resolve, reject) => {
+        pdf.create(_violationPDF(enrollee)).toBuffer((err, buffer) => {
+          resolve(buffer);
+        });
+      });
+      _sendMail({
+        violation,
+        to,
+        date: enrollee.date,
+        nameFirst: enrollee.nameFirst,
+        nameLast: enrollee.nameLast,
+        subject: `${enrollee.nameLast}, ${enrollee.nameFirst} - Violation (${enrollee.violation})`,
+        enrollee,
+        attachments: [
+          {
+            filename: `${enrollee.nameLast}, ${enrollee.nameFirst}.pdf`,
+            content,
+          },
+        ],
       });
     });
-    _sendMail({
-      violation,
-      to,
-      date: enrollee.date,
-      nameFirst: enrollee.nameFirst,
-      nameLast: enrollee.nameLast,
-      subject: `${enrollee.nameLast}, ${enrollee.nameFirst} - Violation (${enrollee.violation})`,
-      enrollee,
-      attachments: [
-        {
-          filename: `${enrollee.nameLast}, ${enrollee.nameFirst}.pdf`,
-          content,
-        },
-      ],
-    });
-  });
+  } catch (e) {
+    console.log(e);
+  }
 };
