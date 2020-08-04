1deefd16fe10247ddfd0e5b150260a1e788a64f4
CMSDev2809 <broc@compliancemonitoringsystems.com>
2020-08-04 11:28:50 -0600

    changes

--- contactList.js
+++ contactList.js
@@ -3,168 +3,150 @@ module.exports = {
     name: "Austin Alt",
     title: "Admin/Accounts Receivable Clerk",
     email: "austin@compliancemonitoringsystems.com",
-    phonenumber: "(406) 529-1789"
+    phonenumber: "(406) 529-1789",
+  },
+  ajohanneck: {
+    name: "Amber Johanneck",
+    title: "24/7 Technician",
+    email: "amber.johanneck@compliancemonitoringsystems.com",
+    phonenumber: "(406) 529-1789",
   },
   anealey: {
     name: "Amber Nealey",
     title: "Admin/Accounts Receivable",
     email: "amber@compliancemonitoringsystems.com",
-    phonenumber: "(406) 529-1789"
+    phonenumber: "(406) 529-1789",
   },
   blavanway: {
     name: "Barry LaVanway",
     title: "Compliance Officer",
     email: "barry@compliancemonitoringsystems.com",
-    phonenumber: "(406) 393-2455"
+    phonenumber: "(406) 393-2455",
   },
   broberts: {
     name: "Broc Roberts",
     title: "Information Technology (IT)",
     email: "broc@compliancemonitoringsystems.com",
-    phonenumber: "(406) 529-1789"
-  },
-  ckeyser: {
-    name: "Chris Keyser",
-    title: "Community Corrections Officer",
-    email: "chris@compliancemonitoringsystems.com",
-    phonenumber: "(406) 274-0155"
+    phonenumber: "(406) 529-1789",
   },
   cmurthy: {
     name: "Courtney Murthy",
     title: "Compliance Officer",
     email: "courtney@compliancemonitoringsystems.com",
-    phonenumber: "(406) 531-9893"
-  },
-  dpierce: {
-    name: "Dennis Pierce",
-    title: "Community Corrections Officer",
-    email: "dennis.pierce@compliancemonitoringsystems.com",
-    phonenumber: "(406) 315-3241"
+    phonenumber: "(406) 531-9893",
   },
   dshepard: {
     name: "Devinne Shepard",
     title: "Compliance Officer",
     email: "devinne@compliancemonitoringsystems.com",
-    phonenumber: "(406) 529-1789"
+    phonenumber: "(406) 529-1789",
   },
   gjacobson: {
     name: "Greg Jacobson",
     title: "Community Supervision Officer",
     email: "greg@compliancemonitoringsystems.com",
-    phonenumber: "(406) 493-5030"
+    phonenumber: "(406) 493-5030",
   },
   chopkins: {
     name: "Cynthia Hopkins",
     title: "Compliance Officer",
     email: "cynthia.hopkins@compliancemonitoringsystems.com",
-    phonenumber: "(406) 315-3241"
+    phonenumber: "(406) 315-3241",
   },
   hroos: {
     name: "Heather Roos",
     title: "Community Corrections Officer",
     email: "heather@communitysupervision.org",
-    phonenumber: "(406) 214-7219"
+    phonenumber: "(406) 214-7219",
   },
   jhenry: {
     name: "Jake Henry",
     title: "Program Manager",
     email: "jake@compliancemonitoringsystems.com",
-    phonenumber: "(406) 531-5242"
+    phonenumber: "(406) 531-5242",
   },
   jmathias: {
     name: "John Mathias",
     title: "Compliance Officer",
     email: "john@compliancemonitoringsystems.com",
-    phonenumber: "(406) 544-5549"
+    phonenumber: "(406) 544-5549",
   },
   jclairmont: {
     name: "Joleen Clairmont",
     title: "Compliance Officer",
     email: "joleen@compliancemonitoringsystems.com",
-    phonenumber: "(406) 493-5030"
+    phonenumber: "(406) 493-5030",
   },
   jsickels: {
     name: "Joe Sickels",
     title: "Lab Manager",
     email: "joe@compliancemonitoringsystems.com",
-    phonenumber: "(406) 370-5262"
+    phonenumber: "(406) 370-5262",
   },
   jtarbert: {
     name: "Jodine Tarbert",
     title: "Chief Executive Officer / Owner",
     email: "jodine@compliancemonitoringsystems.com",
-    phonenumber: "(406) 880-2322"
+    phonenumber: "(406) 880-2322",
   },
   kkeeler: {
     name: "Katie Keeler",
     title: "Compliance Officer",
     email: "katie@compliancemonitoringsystems.com",
-    phonenumber: "(406) 945-9012"
+    phonenumber: "(406) 945-9012",
   },
   mrose: {
     name: "Marisa Rose",
     title: "Compliance Officer",
     email: "marisa@compliancemonitoringsystems.com",
-    phonenumber: "(406) 315-3241"
-  },
-  mjohnson: {
-    name: "Mindy Johnson",
-    title: "Compliance Officer",
-    email: "mindy@compliancemonitoringsystems.com",
-    phonenumber: "(406) 393-2455"
+    phonenumber: "(406) 315-3241",
   },
   mkimmel: {
     name: "Molly Kimmel",
     title: "Compliance Officer",
     email: "molly@compliancemonitoringsystems.com",
-    phonenumber: "(406) 370-5958"
+    phonenumber: "(406) 370-5958",
   },
   rmclaughlin: {
     name: "Ryley McLaughlin",
     title: "Laboratory Technician / Compliance Officer",
     email: "ryley@compliancemonitoringsystems.com",
-    phonenumber: "(406) 529-1789"
+    phonenumber: "(406) 529-1789",
   },
   rkula: {
     name: "Raquel Kula",
     title: "Community Corrections Officer",
     email: "raquel@compliancemonitoringsystems.com",
-    phonenumber: "(406) 529-1213"
-  },
-  stennant: {
-    name: "Sarah Tennant",
-    title: "Sepcimen Collector",
-    email: "sarah@compliancemonitoringsystems.com",
-    phonenumber: "(406) 544-5549"
+    phonenumber: "(406) 529-1213",
   },
   sleddy: {
     name: "Stephen Leddy",
     title: "Chief Operations Officer",
     email: "stephen@compliancemonitoringsystems.com",
-    phonenumber: "(406) 529-1789"
+    phonenumber: "(406) 529-1789",
   },
   tdavis: {
     name: "Tiffany Davis",
     title: "Monitoring Center Supervisor",
     email: "tiffany@compliancemonitoringsystems.com",
-    phonenumber: "(406) 493-4540"
+    phonenumber: "(406) 493-4540",
   },
   tstrub: {
     name: "Tate Strub",
     title: "Compliance Officer",
     email: "tate@compliancemonitoringsystems.com",
-    phonenumber: "(406) 370-4806"
+    phonenumber: "(406) 370-4806",
   },
   vhayden: {
     name: "Virgil Hayden",
     title: "Compliance Officer",
     email: "virgil@compliancemonitoringsystems.com",
-    phonenumber: "(406) 493-2084"
+    phonenumber: "(406) 493-2084",
   },
   wwolfe: {
     name: "William Wolfe",
     title: "Compliance Officer",
     email: "william@compliancemonitoringsystems.com",
-    phonenumber: "(406) 315-3241"
-  }
+    phonenumber: "(406) 315-3241",
+  },
 };
