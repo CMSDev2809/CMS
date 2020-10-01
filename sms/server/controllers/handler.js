586dc44ddbefc487a7dfbb746ea5920e5914144f
broberts2 <broc.roberts@outlook.com>
2020-10-01 13:21:53 -0600

    changes

--- sms/server/controllers/handler.js
+++ sms/server/controllers/handler.js
@@ -2,6 +2,6 @@ const TwilioAPI = require("./handlers/twilioAPI");
 const CalendarAPI = require("./handlers/calendarAPI");
 const KnownSMS = require("./handlers/KnownSMS");
 const SMS = require("./handlers/SMS");
-const UserHandling = require("./handlers/UserHandling");
+const UserHandling = require("./handlers/userHandling");
 
 module.exports = { TwilioAPI, CalendarAPI, SMS, KnownSMS, UserHandling };
