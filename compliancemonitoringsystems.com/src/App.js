7e6cf8976d27e776ecba266109ae742fd2f8c0cf
broberts2 <broc.roberts@outlook.com>
2022-02-24 14:52:47 -0700

    changes

--- compliancemonitoringsystems.com/src/App.js
+++ compliancemonitoringsystems.com/src/App.js
@@ -20,37 +20,52 @@ import News from "./Components/News/news";
 //import News from "./Components/News/glide";
 import { history } from "./store";
 
+function emptyCache() {
+	if ("caches" in window) {
+		caches.keys().then((names) => {
+			// Delete all the cache files
+			names.forEach((name) => {
+				caches.delete(name);
+			});
+		});
+
+		// Makes sure the page reloads. Changes are only visible after you refresh.
+		window.location.reload(true);
+	}
+}
+
 export default class App extends Component {
-  render() {
-    return (
-      <Router history={history}>
-        <div>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/services" component={Services} />
-          <Route exact path="/about_us" component={AboutUs} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/scram" component={Scram} />
-          <Route exact path="/remote_breath" component={RemoteBreath} />
-          <Route exact path="/gps" component={GPS} />
-          <Route exact path="/drug_patch" component={DrugPatch} />
-          <Route exact path="/paternity_testing" component={PaternityTesting} />
-          <Route exact path="/intoxalock" component={Intoxalock} />
-          <Route exact path="/house_arrest" component={HouseArrest} />
-          <Route exact path="/urinalysis" component={Urinalysis} />
-          <Route exact path="/pay" component={PayOnline} />
-          <Route exact path="/referral" component={Refer} />
-          <Route exact path="/news" component={News} />
-          <Route
-            exact
-            path="/careers"
-            component={() => {
-              window.location.href =
-                "https://compliancemonitoringsystems.bamboohr.com/jobs/";
-              return null;
-            }}
-          />
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		emptyCache();
+		return (
+			<Router history={history}>
+				<div>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/services" component={Services} />
+					<Route exact path="/about_us" component={AboutUs} />
+					<Route exact path="/contact" component={Contact} />
+					<Route exact path="/scram" component={Scram} />
+					<Route exact path="/remote_breath" component={RemoteBreath} />
+					<Route exact path="/gps" component={GPS} />
+					<Route exact path="/drug_patch" component={DrugPatch} />
+					<Route exact path="/paternity_testing" component={PaternityTesting} />
+					<Route exact path="/intoxalock" component={Intoxalock} />
+					<Route exact path="/house_arrest" component={HouseArrest} />
+					<Route exact path="/urinalysis" component={Urinalysis} />
+					<Route exact path="/pay" component={PayOnline} />
+					<Route exact path="/referral" component={Refer} />
+					<Route exact path="/news" component={News} />
+					<Route
+						exact
+						path="/careers"
+						component={() => {
+							window.location.href =
+								"https://compliancemonitoringsystems.bamboohr.com/jobs/";
+							return null;
+						}}
+					/>
+				</div>
+			</Router>
+		);
+	}
 }
