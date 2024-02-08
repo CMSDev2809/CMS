948f808284b2e76a8b7d84b2eb145cdd9ee7c9cd
CMSDev2809 <35309574+CMSDev2809@users.noreply.github.com>
2024-02-08 10:40:21 -0700

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,8 +1,4 @@
 git pull origin master
-cd /CMS/compliancemonitoringsystems.com
-npm run build
-rm -rf /var/www/compliancemonitoringsystems.com/html/build
-mv build /var/www/compliancemonitoringsystems.com/html
 cd /CMS/communitysupervision.org
 npm run build
 rm -rf /var/www/communitysupervision.org/html/build
